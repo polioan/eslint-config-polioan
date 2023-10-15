555670bdcd60174f11a3ec705d30e0acc986b496
polioan <rognash.yekan@yandex.ru>
2023-10-15 11:58:08 +0300

    bump version

--- src/configurations/next.ts
+++ src/configurations/next.ts
@@ -1,7 +1,7 @@
 import type { Config } from '../types/config'
 
 const config: Config = {
-  plugins: [],
+  plugins: ['@next/next'],
   rules: {
     '@next/next/google-font-display': ['warn'],
     '@next/next/google-font-preconnect': ['warn'],
